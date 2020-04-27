140ff7299c226b518153f72e1cc58bfffbf79d69
martinuspatandianan1 <63387920+martinuspatandianan1@users.noreply.github.com>
2020-04-27 05:44:09 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,131 @@
+<?php
+$ua ="xxxxxxxxxxxxxxx";
+#
+$url ="https://joinfaucet.com/eth/claim.php?coin=ETH";
+$phpsessid = "cookie: PHPSESSID=xxxxxc";
+$token = "cookie: ETHToken=xxxxcxc";
+#
+$url1 ="https://joinfaucet.com/neo/claim.php?coin=NEO";
+$phpsessid1 = "cookie: PHPSESSID=xxxxxxx";
+$token1 = "cookie: NEOToken=xxxxxxxx";
+#
+$url2 ="https://joinfaucet.com/ltc/claim.php?coin=LTC";
+$phpsessid2 = "cookie: PHPSESSID=xxxxxxx";
+$token2 = "cookie: LTCToken=xxxxxxxx";
+#
+$url3 ="https://joinfaucet.com/eth/claim.php?coin=ETH";
+$phpsessid3 = "cookie: PHPSESSID=xxxxxxx";
+$token3 = "cookie:ETHToken=xxxxxxxx";
+#
+$url4 ="https://joinfaucet.com/trx/claim.php?coin=TRX";
+$phpsessid4 = "cookie: PHPSESSID=xxxxcccc";
+$token4 = "cookie: TRXToken=xxxxxxcc";
+##
+$url5 ="https://joinfaucet.com/zec/claim.php?coin=ZEC";
+$phpsessid5 = "cookie: PHPSESSID=xxxxxxxx";
+$token5 = "cookie: ZECToken=xxxxxxxx";
+##
+$url6 ="https://joinfaucet.com/ltc/claim.php?coin=LTC";
+$phpsessid6 = "cookie: PHPSESSID=xxxxxxxx";
+$token6 = "cookie: LTCToken=xxxxxxxxxx";
+##
+$url7 ="https://joinfaucet.com/trx/claim.php?coin=TRX";
+$phpsessid7 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token7 = "cookie: TRXToken=xxxxxxxxxx";
+##
+$url8 ="https://joinfaucet.com/dash/claim.php?coin=DASH";
+$phpsessid8 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token8 = "cookie: DASHToken=xxxxxxxxxx";
+##
+$url9 ="https://joinfaucet.com/dgb/claim.php?coin=DGB";
+$phpsessid9 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token9 = "cookie: DGBToken=xxxxxxxxxx";
+##
+$url10 ="https://joinfaucet.com/lsk/claim.php?coin=LSK";
+$phpsessid10 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token10 = "cookie: LSKToken=xxxxxxxxxx";
+##
+$url11 ="https://joinfaucet.com/waves/claim.php?coin=WAVES";
+$phpsessid11 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token11 = "cookie: WAVESToken=xxxxxxxxxx";
+##
+$url12 ="https://coincycles.com/ltc/claim.php?coin=LTC";
+$phpsessid12 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token12= "cookie: LTCToken=xxxxxxxxxx";
+#
+$url13 ="https://coincycles.com/bch/claim.php?coin=BCH";
+$phpsessid13 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token13 = "cookie: BCHToken=xxxxxxxxxx";
+##
+$url14 ="https://coincycles.com/doge/claim.php?coin=DOGE";
+$phpsessid14 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token14 = "cookie: DOGEToken=xxxxxxxxxx";
+##
+$url15 ="https://coincycles.com/trx/claim.php?coin=TRX";
+$phpsessid15 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token15 = "cookie: TRXToken=hfK8knJ5u3HAkF14Z6PTU9gsahoReDIw";
+##
+$url16 ="https://coincycles.com/zec/claim.php?coin=ZEC";
+$phpsessid16 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token16 = "cookie: ZECToken=xxxxxxxxxx";
+##
+$url17 ="https://coincycles.com/dgb/claim.php?coin=DGB";
+$phpsessid17 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token17 = "cookie: DGBToken=xxxxxxxxxx";
+##
+$url18 ="https://coincycles.com/bcn/claim.php?coin=BCN";
+$phpsessid18= "cookie: PHPSESSID=xxxxxxxxxx";
+$token18 = "cookie: BCNToken=xxxxxxxxxx";
+##
+$url19 ="https://coincycles.com/xrp/claim.php?coin=XRP";
+$phpsessid19 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token19 = "cookie: XRPToken=xxxxxxxxxx";
+##
+$url20 ="https://coincycles.com/dash/claim.php?coin=DASH";
+$phpsessid20 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token20 = "cookie: DASHToken=xxxxxxxxxx";
+##
+$url21 ="https://coincycles.com/xmr/claim.php?coin=XMR";
+$phpsessid21 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token21 = "cookie: XMRToken=xxxxxxxxxx";
+##
+$url22 ="https://coincycles.com/exs/claim.php?coin=EXS";
+$phpsessid22 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token22 = "cookie: EXSToken=xxxxxxxxxx";
+##
+$url23 ="https://coincycles.com/eth/claim.php?coin=ETH";
+$phpsessid23 = "cookie: PHPSESSID=xxxxxxxxxx";
+$token23 = "cookie: ETHToken=xxxxxxxxxx";
+
+?>
+##
+$url19 ="https://coincycles.com/xrp/claim.php?coin=XRP";
+$phpsessid19 = "cookie: PHPSESSID=xxxxxxxxx";
+$token19 = "cookie: XRPToken=xxxxxxxxxxx";
+##
+$url20 ="https://coincycles.com/dash/claim.php?coin=DASH";
+$phpsessid20 = "cookie: PHPSESSID=xxxxxxxx";
+$token20 = "cookie: DASHToken=xxxxxxxxx";
+##
+$url21 ="https://coincycles.com/xmr/claim.php?coin=XMR";
+$phpsessid21 = "cookie: PHPSESSID=xxxxxxxx";
+$token21 = "cookie: XMRToken=xxxxxxxxx";
+##
+$url22 ="https://coincycles.com/exs/claim.php?coin=EXS";
+$phpsessid22 = "cookie: PHPSESSID=xxxxxxxx";
+$token22 = "cookie: EXSToken=xxxxxxxxxx";
+##
+$url23 ="https://coincycles.com/eth/claim.php?coin=ETH";
+$phpsessid23 = "cookie: PHPSESSID=xxxxxxxxx";
+$token23 = "cookie: ETHToken=xxxxxxxxxx";
+
+?>
+.php?coin=EXS";
+$phpsessid22 = "cookie: PHPSESSID=608586f1e0dd90384cf4d8fd1b4e2d95";
+$token22 = "cookie: EXSToken=PAoU7xJdUdL09yup2249XaDODLbmyGPB";
+##
+$url23 ="https://coincycles.com/eth/claim.php?coin=ETH";
+$phpsessid23 = "cookie: PHPSESSID=608586f1e0dd90384cf4d8fd1b4e2d95";
+$token23 = "cookie: ETHToken=XQyc4EpE9wE6rbkCJPG0R1LtRH6p84AI";
+
+?>
